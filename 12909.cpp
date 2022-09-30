d6f16b255368d36957d5515c9c92042a4e4a9185
Riza <wowns226@naver.com>
2022-09-30 15:54:25 +0900

    [Programmers] (스택/큐) 올바른 괄호

---
+++ 12909.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define FASTIO                             \
+    std::ios_base::sync_with_stdio(false); \
+    std::cin.tie(0);                       \
+    std::cout.tie(0)
+#define ENDL '\n'
+
+/***************************************************************************************
+ *   * 링크 : https://school.programmers.co.kr/learn/courses/30/lessons/12909
+ ***************************************************************************************/
+
+bool isFirstClose() {}
+
+using namespace std;
+bool solution(string s) {
+    if (s.front() == ')' || s.back() == '(') {
+        return false;
+    }
+
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+            st.push('(');
+        } else {  // s[i]==')'
+            if (st.top() == '(') {
+                st.pop();
+            }
+        }
+    }
+
+    if (st.empty()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    FASTIO;
+    vector<string> v;
+    v.push_back("()()");
+    v.push_back("(())()");
+    v.push_back(")()(");
+    v.push_back("(()(");
+    int test = 4;
+    for (int i = 0; i < v.size(); i++) {
+        cout << solution(v[i]) << ENDL;
+    }
+    return 0;
+}
